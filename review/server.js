2cb4a2f3c91017e6788145b04a424f92bdecd450
wnstndks <anjs9912@gmail.com>
2024-04-12 17:29:24 +0900

    페이지 만들기

--- review/server.js
+++ review/server.js
@@ -1,10 +1,20 @@
 const express = require('express')
 const app = express()
+app.use(express.static(__dirname+'/public'));
+
+
+
+
 
 app.listen(8080, () => {
     console.log('http://localhost:8080 에서 서버 실행중')
 })
 
 app.get('/', (요청, 응답) => {
-  응답.send('오랜만')
+  응답.sendFile(__dirname+'/main.html')
 }) 
+
+// 새로운 페이지 만들고 싶을 때
+app.get('/new',(req,res)=>{
+    req.send('오늘부터 다시 node 시작해서 재밌다.')
+})
