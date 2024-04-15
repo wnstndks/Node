a61e9843ecbf721fd69899baba96e469685800c1
wnstndks <anjs9912@gmail.com>
2024-04-15 12:09:12 +0900

    ejs

--- review/server.js
+++ review/server.js
@@ -1,36 +1,46 @@
-const express = require('express')
-const app = express()
-app.use(express.static(__dirname+'/public'));
+const express = require("express");
+const app = express();
+app.use(express.static(__dirname + "/public"));
+// ejs 세팅 - 템플릿 엔진 쓰기위함 -> html 파일안에 데이터들을 꽂을수 있음
+app.set("view engine", "ejs");
+const { MongoClient } = require("mongodb");
 
-const { MongoClient } = require('mongodb')
+let db;
+const url =
+  "mongodb+srv://admin:dwjtk2718@cluster0.nmfemuv.mongodb.net/?retryWrites=true&w=majority";
+new MongoClient(url)
+  .connect()
+  .then((client) => {
+    console.log("DB연결성공");
+    db = client.db("reviewdatabase");
 
-
-let db
-const url = "mongodb+srv://admin:dwjtk2718@cluster0.nmfemuv.mongodb.net/?retryWrites=true&w=majority"
-new MongoClient(url).connect().then((client)=>{
-  console.log('DB연결성공')
-  db = client.db('reviewdatabase')
-
-  app.listen(8080, () => {
-    console.log('http://localhost:8080 에서 서버 실행중')
+    app.listen(8080, () => {
+      console.log("http://localhost:8080 에서 서버 실행중");
+    });
   })
+  .catch((err) => {
+    console.log(err);
+  });
 
-}).catch((err)=>{
-  console.log(err)
-})
-
-app.get('/', (요청, 응답) => {
-  응답.sendFile(__dirname+'/main.html')
-}) 
-
+app.get("/", (요청, 응답) => {
+  응답.sendFile(__dirname + "/main.html");
+});
 
 // 새로운 페이지 만들고 싶을 때
-app.get('/news',(req,res)=>{
-    db.collection('reviewpost').insertOne({title:'시작',content:'이게node다'})
-    // req.send('오늘부터 다시 node 시작해서 재밌다.')
-})
+app.get("/news", (req, res) => {
+  db.collection("reviewpost").insertOne({
+    title: "시작",
+    content: "이게node다",
+  });
+  // req.send('오늘부터 다시 node 시작해서 재밌다.')
+});
 
-app.get('/list',async(req,res)=>{
+app.get("/post", async (req, res) => {
+  let result = await db.collection("reviewpost").find().toArray();
+  res.send(result[1].content);
+});
+
+app.get("/list", async(req, res) => {
   let result= await db.collection('reviewpost').find().toArray()
-  res.send(result[1].content)
-})
+  res.render("list.ejs",{글목록: result});
+});
